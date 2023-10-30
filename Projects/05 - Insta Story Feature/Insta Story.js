2c6d81a965a9bb10f40149c1086f2087c1edee94
M Hussnain Fareed <hussnainfareed@outlook.com>
2023-10-30 15:53:12 +0500

    05 - Insta Story Project completed

--- Projects/05 - Insta Story Feature/Insta Story.js
+++ Projects/05 - Insta Story Feature/Insta Story.js
@@ -14,17 +14,19 @@ const data = [
     },
     {
         dp: "https://images.unsplash.com/photo-1440133197387-5a6020d5ace2?auto=format&fit=crop&q=80&w=387&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        story: "https://images.unsplash.com/photo-1533093818119-ac1fa47a6d59?auto=format&fit=crop&q=80&w=387&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        story: "https://images.unsplash.com/photo-1698281964068-be15d9e587bc?auto=format&fit=crop&q=80&w=1948&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         dp: "https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&q=80&w=461&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         story: "https://images.unsplash.com/photo-1530047139082-5435ca3c4614?auto=format&fit=crop&q=80&w=870&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
-];
-let display = "";
+    }];
+
+let users = "";
+let stories_section = document.querySelector("#stories_container");
+let story_screen = document.querySelector("#displayStory");
 
 data.forEach(function (obj, i) {
-    display += 
+    users += 
     `
     <div class="story_status_border">
         <div class="bg">
@@ -35,5 +37,18 @@ data.forEach(function (obj, i) {
     </div>
     `;
 });
+stories_section.innerHTML = users;  //users div added.
 
-document.querySelector("#stories_container").innerHTML = display;
+stories_section.addEventListener("click", function(pos){
+    // console.log((pos.target.id).slice(3));
+    
+    let user_story = data[(pos.target.id).slice(3) - 1].story;
+
+    story_screen.style.display = "block";
+    story_screen.style.backgroundImage = `url(${user_story})`;
+
+    setTimeout(() => {
+        story_screen.style.display = "none";
+    }, 3000);
+
+});
